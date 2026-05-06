47c0558c06fc2b3e901118ddd558ae03cd03daa5
ViniNovack <vininomn@gmail.com>
2026-05-06 16:10:05 -0300

    Atualização aula 1

--- projeto1.java
+++ projeto1.java
@@ -1,5 +1,5 @@
 public class projeto1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Hello, word");
     }
 }
