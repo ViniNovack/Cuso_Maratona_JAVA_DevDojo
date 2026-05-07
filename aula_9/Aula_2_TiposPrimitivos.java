0314e2355a2a16f3053f5e8b084f52fc02e137b9
ViniNovack <vininomn@gmail.com>
2026-05-07 15:31:09 -0300

    Atualização

--- aula_9/Aula_2_TiposPrimitivos.java
+++ aula_9/Aula_2_TiposPrimitivos.java
@@ -3,6 +3,9 @@ package aula_9;
 public class Aula_2_TiposPrimitivos {
     public static void main(String[] args) {        // psvm     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         int idade = 10;                             // Exemplo de variavel
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        
         System.out.println("Ele tem " + idade + " anos.\n");
     }
 }
