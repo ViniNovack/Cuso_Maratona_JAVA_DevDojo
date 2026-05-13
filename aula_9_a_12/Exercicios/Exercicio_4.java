628698ad6d26feab43f43fed40af88cc3db1ebb5
ViniNovack <vininomn@gmail.com>
2026-05-13 17:23:57 -0300

    Termino de todos os exercicios

---
+++ aula_9_a_12/Exercicios/Exercicio_4.java
@@ -0,0 +1,13 @@
+package aula_9_a_12.Exercicios;
+
+public class Exercicio_4 {
+    public static void main(String[] arsg){
+    int idade = 18;
+    int carteira = 100;
+    
+    boolean podeEntrar = idade >= 18;
+    boolean conseguePagar = carteira >= 150;
+
+    System.out.println("O cliente tem idade para entrar? " + podeEntrar + "\nO cliente consegue pagar? " + conseguePagar);
+    }
+}
