9fb0fc2d34665b4f53680c60856f9f6198f79353
ViniNovack <vininomn@gmail.com>
2026-05-14 16:59:40 -0300

    Atualização

---
+++ aulas_15_a_/Aulas/Aula_15.java
@@ -0,0 +1,12 @@
+package aulas_15_a.Aula_15;
+
+public class aula_15 {
+    public static void main(String[], args){
+        
+        int numero01 = 10;
+        int numero02 = 20;
+
+        System.out.println(numero01 + numero02 + " VALOR " + numero01 + numero02);
+
+    }   
+}
