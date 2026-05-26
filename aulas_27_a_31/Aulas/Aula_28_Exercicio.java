3226f118e9150fd8859cdbacd1571be5da6923bd
Vinícius <vininomn@gmail.com>
2026-05-26 09:20:35 -0300

    Atualização

---
+++ aulas_27_a_31/Aulas/Aula_28_Exercicio.java
@@ -0,0 +1,16 @@
+package aulas_27_a_31.Aulas;
+
+public class Aula_28_Exercicio {
+    public static void main(String[] args){
+        int cont = 0;
+        while(cont <= 100){
+            if(cont % 2 == 0){
+                System.out.println(cont);
+                cont +=1;
+            }
+            else{
+                cont +=1;
+            }
+        }
+    }
+}
