6488f58347ff4c8b6d3f39bb026d035d093265ea
ViniNovack <vininomn@gmail.com>
2026-05-29 08:28:35 -0300

    Atualizações

--- aulas_27_a_31/Aulas/Aula_27.java
+++ aulas_27_a_31/Aulas/Aula_27.java
@@ -1,4 +1,4 @@
-package aulas_27_a_31.Aulas;
+// package aulas_27_a_31.Aulas;
 
 //______________________LOOPS______________________
 public class Aula_27{
@@ -6,20 +6,20 @@ public class Aula_27{
         // WHILE
         int cont = 0;
         while (cont <= 10){
-            System.out.println(++cont);
+            System.out.println(cont++);
         }
 
         System.out.println();
         // DO WHILE: Ele tem a caracteristica de executar pelomenos uma vez não importando a condição, mas ele só vai continuar se a condição for verdadeira
-        cont = 3;
+        cont = 0;
         do{
-            System.out.println("Entrou no DO" + ++cont);
-        } while (cont < 10);
+            System.out.println("Entrou no DO " + cont++);
+        } while (cont <= 10);
 
         System.out.println();
         // FOR
-        for(int i=0; i < 10; i++){
-            System.out.println("For"+i);
+        for(int i=0; i < 11; i++){
+            System.out.println("For "+i);
         }
     }
 }
