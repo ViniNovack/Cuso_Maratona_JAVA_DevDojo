17700ec5ff2d5aefc1c4b214958167fd66d4df22
ViniNovack <vininomn@gmail.com>
2026-05-29 09:27:29 -0300

    Atualização aula 31

--- aulas_27_a_31/Aulas/Aula_31.java
+++ aulas_27_a_31/Aulas/Aula_31.java
@@ -1,3 +1,16 @@
 public class Aula_31 {
-    
+    public static void main(String[] args){
+        double valorTotal = 30_000;
+        double valorAte = 1000;
+        double valorParcela;
+
+        for(int c = (int)valorTotal; c >= 1; c--){
+            valorParcela = valorTotal / c;
+            if(valorParcela < valorAte){
+                continue;
+            } else{
+                System.out.println("Parcela " + c + " = " + valorParcela);
+            }
+        }
+    }
 }
