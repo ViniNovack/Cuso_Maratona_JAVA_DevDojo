564a012acdc7433e3e78cf431548345b2f268c4d
ViniNovack <vininomn@gmail.com>
2026-06-03 15:35:41 -0300

    Atualização

--- aulas_32_a_35/Aulas/Aula_35.java
+++ aulas_32_a_35/Aulas/Aula_35.java
@@ -1,9 +1,9 @@
 public class Aula_35 {
     public static void main(String[] args){
         //Formas de se iniciar uma lista:
-        int[] numeros1 = new int[3];
+        //int[] numeros1 = new int[3];
         int[] numeros2 = {1, 2, 3, 4, 5};
-        int[] numeros3 = new int[]{5, 4, 3, 2, 1};
+        //int[] numeros3 = new int[]{5, 4, 3, 2, 1};
 
         //Para passar os valores da lista com for se usa a seguinte logica:
         for(int num : numeros2){
