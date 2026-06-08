d5eeee9e7d6124528b3aa2aae9039c8c8e30be8e
ViniNovack <vininomn@gmail.com>
2026-06-08 18:05:40 -0300

    Atualização

--- aulas_32_a_35/Exercicios/Exercicio_1.java
+++ aulas_32_a_35/Exercicios/Exercicio_1.java
@@ -1,5 +1,21 @@
 public class Exercicio_1 {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        int[] notas = {5, 8, 7, 8};
+        double media;
+        int soma = 0;
+        int cont = 0;
+        for(int num : notas){
+            soma +=num;
+            cont++;
+        }
+        media = (soma / cont);
+
+        if(media >= 7){
+            System.out.println("Paranens, você foi aprovado " + "Nota: " + media);
+        } else if(7 > media && media >= 5){
+            System.out.println("Você ficou de recuperação " + "Nota: " + media);
+        } else if(5 > media){
+            System.out.println("Você foi reprovado " + "Nota: " + media);
+        }
     }
 }
