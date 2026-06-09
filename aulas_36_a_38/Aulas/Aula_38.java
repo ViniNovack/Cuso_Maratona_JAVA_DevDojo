1236424a0466b7335cc6edef6275f2fda54f096f
ViniNovack <vininomn@gmail.com>
2026-06-09 15:16:34 -0300

    Aulas 36 a 38

---
+++ aulas_36_a_38/Aulas/Aula_38.java
@@ -0,0 +1,20 @@
+public class Aula_38 {
+    public static void main(String[] args){
+        int[] arry = {1, 2, 3};
+
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = arry;
+        arrayInt[2] = new int[]{1, 2, 3, 4, 5, 6};
+
+        int[][] arrayInt2 = {{0, 0}, {1, 2, 3}, {1, 2, 3, 4, 5, 6}};
+
+        for(int[] arrayBase : arrayInt2){
+            System.out.println("\n-------");
+            for(int num : arrayBase){
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
