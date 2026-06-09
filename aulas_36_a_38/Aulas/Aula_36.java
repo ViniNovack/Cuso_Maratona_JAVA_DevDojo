f0ce1e271859e6430d575fa555b127d9b9d83c12
ViniNovack <177799919+ViniNovack@users.noreply.github.com>
2026-06-09 09:32:05 -0300

    aula 36

--- aulas_36_a_38/Aulas/Aula_36.java
+++ aulas_36_a_38/Aulas/Aula_36.java
@@ -1,3 +1,14 @@
 public class Aula_36 {
-    
+    public static void main(String[] args){
+        // ARRAYS
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 30;
+        for(int i = 0; i < dias.length; i++){
+            for(int j = 0; j < dias[0].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+    }
 }
