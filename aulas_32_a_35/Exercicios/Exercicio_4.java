43f39816d95e2f3b124a8814932365bb6656136a
ViniNovack <vininomn@gmail.com>
2026-06-08 21:51:12 -0300

    Desenvolvimento exercicio 4

--- aulas_32_a_35/Exercicios/Exercicio_4.java
+++ aulas_32_a_35/Exercicios/Exercicio_4.java
@@ -1,5 +1,6 @@
 public class Exercicio_4 {
     public static void main(String[] args){
-        ;;
+        String[] lista = {"Vinícius", "Guilherme", "Gabriel", "Carol", "Lucas", "Isak", "Arthur"};
+        String nome = "Arthur";
     }
 }
