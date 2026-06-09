0f4e1271c855e7b447a02e9fd2f504188d2943ce
ViniNovack <177799919+ViniNovack@users.noreply.github.com>
2026-06-09 09:14:32 -0300

    Termino do exercício 4

--- aulas_32_a_35/Exercicios/Exercicio_4.java
+++ aulas_32_a_35/Exercicios/Exercicio_4.java
@@ -1,6 +1,19 @@
 public class Exercicio_4 {
     public static void main(String[] args){
         String[] lista = {"Vinícius", "Guilherme", "Gabriel", "Carol", "Lucas", "Isak", "Arthur"};
-        String nome = "Arthur";
+        String nome = "Lucas";
+        boolean encontrado = false;
+        for(int i = 0; i < lista.length; i++){
+            if(nome.equals(lista[i])){
+                System.out.println("O nome " + nome + " esta na posição " + (i + 1) + " da lista.");
+                encontrado = true;
+                break;
+            } else{
+                continue;
+            }
+        }
+        if(!encontrado){                                                               // OR encontrado == false
+            System.out.println("O nome " + nome + " não esta na lista.");
+        }
     }
 }
